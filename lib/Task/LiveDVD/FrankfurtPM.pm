b470913f8ee24d159b6a02c771eea8366c9e5c77
reneeb <github@renee-baecker.de>
2013-02-25 16:51:17 +0100

    initial commit

---
+++ lib/Task/LiveDVD/FrankfurtPM.pm
@@ -0,0 +1,22 @@
+package Task::LiveDVD::FrankfurtPM;
+
+# ABSTRACT: All these modules are used on LiveDVDs we give away
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+q~A great start to play with Perl~;
+
+__END__
+
+=head1 MODULES REQUIRED
+
+=over 4
+
+=item * Task::Kensho
+
+=item 
+
+=back
