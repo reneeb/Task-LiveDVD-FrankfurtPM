b4d91fa94928bd865ccbf58d7dbf4020e3947997
reneeb <github@renee-baecker.de>
2013-02-26 13:27:22 +0100

    add list of dists that are prereqs for this task module

--- lib/Task/LiveDVD/FrankfurtPM.pm
+++ lib/Task/LiveDVD/FrankfurtPM.pm
@@ -1,6 +1,6 @@
 package Task::LiveDVD::FrankfurtPM;
 
-# ABSTRACT: All these modules are used on LiveDVDs we give away
+# ABSTRACT: All these modules are installed on LiveDVDs we give away
 
 use strict;
 use warnings;
@@ -15,12 +15,878 @@ __END__
 
 =over 4
 
+=item * Alien::wxWidgets
+
+building, finding and using wxWidgets binaries
+
+=item * Alien::LibXML
+
+install the C libxml2 library on your system
+
 =item * Task::Kensho
+
+A glimpse at an Enlightened Perl.
+
+=item * Task::Kensho::OOP
+
+A glimpse at an Enlightened Perl (OOP modules).
+
+=item * Task::Kensho::WebDev
+
+A glimpse at an Enlightened Perl (web development).
+
+=item * Task::Kensho::Scalability
+
+A glimpse at an Enlightened Perl (scalability tools).
+
+=item * Task::Kensho::Logging
+
+A glimpse at an Enlightened Perl (Logging modules).
+
+=item * Task::Kensho::Hackery
+
+A glimpse at an Enlightened Perl (script hackery).
+
+=item * Task::Kensho::Async
+
+A glimpse at an Enlightened Perl (Async Programming).
+
+=item * Task::Kensho::Config
+
+A glimpse at an Enlightened Perl (Config-handling Modules).
+
+=item * Task::Kensho::ExcelCSV
+
+A glimpse at an Enlightened Perl (modules for Excel / CSV files).
+
+=item * Task::Kensho::Exceptions
+
+A glimpse at an Enlightened Perl (exception handling modules).
+
+=item * Task::Kensho::Toolchain
+
+A glimpse at an Enlightened Perl (basic toolchain).
+
+=item * Task::Kensho::CLI
+
+A glimpse at an Enlightened Perl (CLI Tools).
+
+=item * Task::Kensho::XML
+
+A glimpse at an Enlightened Perl (XML).
+
+=item * Task::Kensho::Dates
+
+A glimpse at an Enlightened Perl (Date handling).
+
+=item * Task::Kensho::DBDev
+
+A glimpse at an Enlightened Perl (DB development).
+
+=item * Task::Kensho::Email
+
+A glimpse at an Enlightened Perl (email modules).
+
+=item * Task::Kensho::WebCrawling
+
+A glimpse at an Enlightened Perl (web crawling).
+
+=item * Task::Kensho::Testing
+
+A glimpse at an Enlightened Perl (testing tools).
+
+=item * Task::Kensho::ModuleDev
+
+A glimpse at an Enlightened Perl (module development tools).
+
+=item * MooseX::Method::Signatures
+
+Method declarations with type constraints and no source filter
 
 =item * Mojolicious
 
-=item * Dancer
+Real-time web framework
 
-=item * 
+=item * Mojolicious::Command::deploy
+
+Deployment command
+
+=item * Mojolicious::Plugin::Humane
+
+Mojolicious integration for humane.js
+
+=item * Mojolicious::Plugin::Geo
+
+Mojolicious Plugin
+
+=item * Mojolicious::Plugin::DomIdHelper
+
+Mojolicious plugin to generate DOM IDs and CSS class names from your ORM objects
+
+=item * Mojolicious::Plugin::OAuth2
+
+Authenticate against OAuth2 providers
+
+=item * Mojolicious::Plugin::ParamLogger
+
+Log request parameters
+
+=item * Mojolicious::Plugin::DOCRenderer
+
+Browses online documentation of your Mojolicious application
+
+=item * Mojolicious::Plugin::ParamCondition
+
+Request parameter condition plugin
+
+=item * Mojolicious::Plugin::YamlConfig
+
+YAML Configuration Plugin for Mojolicious
+
+=item * Mojolicious::Command::secret
+
+Create an application secret() consisting of random bytes
+
+=item * Mojolicious::Plugin::DigestAuth
+
+HTTP Digest Authentication for Mojolicious
+
+=item * Mojolicious::Plugin::Recaptcha
+
+ReCaptcha plugin for Mojolicious framework
+
+=item * Mojolicious::Plugin::ProRenderer
+
+HTML::Tempate::Pro render plugin for Mojolicious
+
+=item * Mojolicious::Plugin::SMS
+
+Easy SMS sending from Mojolicious apps
+
+=item * Mojolicious::Plugin::ServerSession
+
+Perl extension to enable an additional server side session in mojolicious
+
+=item * Mojolicious::Plugin::SassRenderer
+
+Sass Renderer Plugin for Mojolicious
+
+=item * Mojolicious::Plugin::BasicAuth
+
+Basic HTTP Auth Helper
+
+=item * Mojolicious::Plugin::BrowserDetect
+
+HTTP::BrowserDetect integration for Mojolicious
+
+=item * Mojolicious::Plugin::PlackMiddleware
+
+Plack::Middleware inside Mojolicious
+
+=item * Mojolicious::Plugin::Subdispatch
+
+create requests to your Mojolicious actions
+
+=item * Mojolicious::Plugin::ZombieApocalypse
+
+Detect Zombie apocalypse
+
+=item * Mojolicious::Plugin::ConsoleLogger
+
+Console logging in your browser
+
+=item * Mojolicious::Plugin::ParamsAuth
+
+URL Parameters Auth Helper
+
+=item * Mojolicious::Plugin::Webtail
+
+display tail to your browser
+
+=item * Mojolicious::Plugin::Gravatar
+
+Globally Recognized Avatars for Mojolicious
+
+=item * Mojolicious::Plugin::WriteExcel
+
+write Excel spreadsheets from Mojolicious
+
+=item * Mojolicious::Plugin::HTTPStatusRenderer
+
+HTTP status renderer plugin
+
+=item * Mojolicious::Plugin::FormFields
+
+Use objects and data structures in your forms
+
+=item * Mojolicious::Plugin::InstallablePaths
+
+Easy installation configuration for Mojolicious apps
+
+=item * Mojolicious::Plugin::UrlWith
+
+Preserve parts of the url
+
+=item * Mojolicious::Plugin::Cache
+
+Mojolicious plugin for caching
+
+=item * Mojolicious::Plugin::Bundle
+
+Collection of mojolicious plugins
+
+=item * Mojolicious::Plugin::Captcha::reCAPTCHA
+
+use Captcha::reCAPTCHA in Mojolicious apps
+
+=item * Mojolicious::Plugin::HTMLTemplateRenderer
+
+Mojolicious Plugin to render templates with HTML::Template
+
+=item * Mojolicious::Plugin::Directory
+
+Serve static files from document root with directory index
+
+=item * mojolicious::plugin::page::navigator
+
+Page Navigator plugin for Mojolicious
+
+=item * Mojolicious::Plugin::JSUrlFor
+
+Mojolicious "url_for" helper for javascript
+
+=item * Mojolicious::Plugin::AutoRoute
+
+Mojolicious Plugin to create routes automatically
+
+=item * Mojolicious::Plugin::TweetButton
+
+TweetButton plugin for Mojolicious
+
+=item * Mojolicious::Plugin::HamlRenderer
+
+Haml renderer for Mojolicious
+
+=item * Mojolicious::Plugin::HTMLLint
+
+HTML::Lint support for Mojolicious
+
+=item * mojolicious::lexicon
+
+parse lexemes from Mojolicious template
+
+=item * Mojolicious::Plugin::EventSource
+
+A plugin to make it eazy to use EventSource with Mojolicious
+
+=item * Mojolicious::Plugin::RenderFile
+
+"render_file" helper for Mojolicious
+
+=item * Mojolicious::Plugin::Message::Locale
+
+Mojolicious Plugin
+
+=item * Mojolicious::Plugin::ValidateTiny
+
+Lightweight validator for Mojolicious
+
+=item * Mojolicious::Plugin::SQLMaker
+
+The great new Mojolicious::Plugin::SQLMaker!
+
+=item * Mojolicious::Plugin::JSONP
+
+Render JSONP with transparent fallback to JSON
+
+=item * Mojolicious::Plugin::SslAuth
+
+SSL client certificate auth helper
+
+=item * Mojolicious::Plugin::PPI
+
+Mojolicious Plugin for Rendering Perl Code Using PPI
+
+=item * Mojolicious::Plugin::MarkaplRenderer
+
+Markapl template plugin for Mojolicious
+
+=item * Mojolicious::Plugin::ValidateMoose
+
+Can validate using Moose objects
+
+=item * Mojolicious::Plugin::MozPersona
+
+Minimalistic integration of Mozillas "Persona" authentication system in Mojolicious apps
+
+=item * Padre::Plugin::Mojolicious
+
+Mojolicious support for Padre
+
+=item * Mojolicious::Plugin::ParamExpand
+
+Use objects and data structures in your forms
+
+=item * Mojolicious::Plugin::CHI
+
+Use CHI caches within Mojolicious
+
+=item * Mojolicious::Plugin::I18N
+
+Internationalization Plugin for Mojolicious 3.x and higher
+
+=item * Mojolicious::Plugin::DSC
+
+use DBIx::Simple::Class in your application.
+
+=item * Mojolicious::Plugin::Database
+
+"proper" handling of DBI based connections in Mojolicious
+
+=item * Mojolicious::Plugin::Web::Auth
+
+Authentication plugin for Mojolicious
+
+=item * Mojolicious::Plugin::Piwik
+
+Use Piwik in Mojolicious
+
+=item * Mojolicious::Plugin::Util::Endpoint
+
+Use template URIs in Mojolicious
+
+=item * Mojolicious::Plugin::Mail
+
+Mojolicious Plugin for send mail
+
+=item * Mojolicious::Plugin::Wolowitz
+
+Mojo I18n with Locale::Wolowitz
+
+=item * Mojolicious::Plugin::Resque
+
+Mojolicious helper for sending jobs to a Resque queue.
+
+=item * Mojolicious::Plugin::Mason1Renderer
+
+Mason 1 (aka HTML::Mason 1.x) Renderer Plugin.
+
+=item * Mojolicious::Plugin::RedirectHost
+
+Redirects requests from mirrors to the main host (useful for SEO)
+
+=item * Mojolicious::Plugin::SimpleSession
+
+Exceedingly Simple Mojolicious Sessions
+
+=item * Mojolicious::Plugin::GroupedParams
+
+grouped params from query.
+
+=item * Mojolicious::Plugin::ConfigRoutes
+
+is a Perl-ish configuration of routes plugin.
+
+=item * Mojolicious::Command::generate::bootstrap
+
+Include Twitters Bootstrap
+
+=item * Mojolicious::Plugin::Mason2Renderer
+
+Mason 2 (aka Mason 2.x) Renderer Plugin.
+
+=item * Mojolicious::Plugin::MountPSGI
+
+Mount PSGI apps
+
+=item * Mojolicious::Command::deploy::heroku
+
+Deploy to Heroku
+
+=item * Mojolicious::Plugin::YubiVerify
+
+Verify YubiKey one time passwords.
+
+=item * Mojolicious::Plugin::DeCSRF
+
+Defend from CSRF attacks centrally.
+
+=item * Mojolicious::Plugin::CaptchaRenderer
+
+captcha renderer for Mojolicious framework
+
+=item * Mojolicious::Plugin::Proxy
+
+Proxy requests
+
+=item * Mojolicious::Plugin::CSRFProtect
+
+Fully protects you from CSRF attacks
+
+=item * Mojolicious::Plugin::LinkedContent
+
+manage linked css and js
+
+=item * Mojolicious::Plugin::ShareHelpers
+
+A Mojolicious Plugin for generate share urls, buttons and meta for Twitter, Facebook, Google Buzz, VKontakte, MyMailRU and Google Plus
+
+=item * Mojolicious::Plugin::Mongodb
+
+Use MongoDB in Mojolicious
+
+=item * Mojolicious::Plugin::Ident
+
+Mojolicious plugin to interact with a remote ident service
+
+=item * Mojolicious::Plugin::DBIxTransactionManager
+
+DBIx::TransactionManager for Mojolicious's plugin
+
+=item * Mojolicious::Plugin::RequestBase
+
+Support setting base in frontend proxy
+
+=item * Mojolicious::Plugin::Toto
+
+A simple tab and object based site structure
+
+=item * Mojolicious::Plugin::Authentication
+
+A plugin to make authentication a bit easier
+
+=item * Mojolicious::Plugin::MethodOverride
+
+Simulate HTTP Verbs
+
+=item * Mojolicious::Plugin::MySQLViewerLite
+
+Mojolicious plugin to display MySQL database information on browser
+
+=item * Mojolicious::Plugin::Redis
+
+Use Redis in Mojolicious
+
+=item * Mojolicious::Plugin::TtRenderer
+
+Template Renderer Plugin
+
+=item * Mojolicious::Plugin::MailException
+
+Mojolicious plugin to send crash information by email
+
+=item * Mojolicious::Command::generate::nifty_app
+
+Generate a simple application with DBIx::Class authentication.
+
+=item * Mojolicious::Plugin::BlogSpam
+
+Check your comments using BlogSpam
+
+=item * Mojolicious::Plugin::Disqus
+
+Interface with Disqus comments from your Mojolicious app
+
+=item * Mojolicious::Plugin::SQLiteViewerLite
+
+Mojolicious plugin to display SQLite database information on browser
+
+=item * Mojolicious::Plugin::Libravatar
+
+Access the Libravatar API in Mojolicious.
+
+=item * Mojolicious::Plugin::WWWSession
+
+Use WWWW::Session with Mojolicious
+
+=item * Mojolicious::Plugin::Pedro
+
+A web-based Perl editor Mojolicious plugin
+
+=item * Mojolicious::Plugin::OnFork
+
+Do Something Whenever a Worker Starts
+
+=item * Mojolicious::Plugin::AnyData
+
+Use and handle any data like a database source in test mode
+
+=item * Mojolicious::Plugin::JavaScript::Console
+
+use the JavaScript console from Mojolicious applications
+
+=item * Mojolicious::Plugin::Sugar
+
+Some sweet stuff for Mojolicious
+
+=item * Mojolicious::Plugin::BasicAuthPlus
+
+Basic HTTP Auth Helper Plus
+
+=item * Mojolicious::Plugin::KossyValidator
+
+根据 Kossy 中的 Validator 移植过来的模块
+
+=item * Mojolicious::Plugin::CSRFDefender
+
+Defend CSRF automatically in Mojolicious Application
+
+=item * Mojolicious::Plugin::CountryDropDown
+
+Provide a localizable dropdown where users can select a country
+
+=item * Mojolicious::Plugin::Authorization
+
+A plugin to make authorization a bit easier
+
+=item * Mojolicious::Plugin::Bcrypt
+
+bcrypt your passwords!
+
+=item * Mojolicious::Plugin::Dotcloud
+
+A quick and dirty wrapper around DotCloud::Environment
+
+=item * Mojolicious::Command::generate::bootstrap_app
+
+Generate a simple application with DBIx::Class authentication.
+
+=item * Mojolicious::Command::listdeps
+
+Command to list dependencies in a Mojolicious project
+
+=item * Mojolicious::Plugin::AccessLog
+
+AccessLog Plugin
+
+=item * Mojolicious::Plugin::Vparam
+
+Mojolicious plugin validator for GET/POST data.
+
+=item * Mojolicious::Plugin::Facebook
+
+=item * MojoX::JSON::RPC
+
+Perl implementation of JSON-RPC 2.0 protocol for Mojolicious
+
+=item * MojoX::AIO
+
+Asynchronous File I/O for Mojolicious
+
+=item * Mojolicious::Plugin::Human
+
+Helpers to print values as human readable form.
+
+=item * IO::Async::Loop::Mojo
+
+use IO::Async with Mojolicious
+
+=item * MojoX::Ping
+
+asynchronous ping with Mojolicious (DEPRECATED).
+
+=item * MojoX::Log::Log4perl
+
+Log::Log4perl logging for Mojo/Mojolicious
+
+=item * MojoX::Logite
+
+A simple Mojo::Log implementation which logs to an SQLite database
+
+=item * Mojo::Server::FastCGI
+
+FastCGI Server
+
+=item * MojoX::Auth::Simple
+
+Perl extension for simple login authentication for Mojolicious web applications
+
+=item * MojoX::Validator
+
+Data validator
+
+=item * Mojo::TFTPd
+
+Trivial File Transfer Protocol daemon
+
+=item * MojoX::Renderer::HTC
+
+HTML::Template::Compiled renderer for Mojo
+
+=item * Mojo::Facebook
+
+Talk with Facebook
+
+=item * MojoX::Renderer::WriteExcel
+
+emit Excel spreadsheets from Mojo
+
+=item * MojoX::Renderer::Xslate
+
+Text::Xslate renderer for Mojo
+
+=item * MojoX::Session
+
+Session management for Mojo
+
+=item * Task::Dancer
+
+Dancer in a box
+
+=item * Starman
+
+High-performance preforking PSGI/Plack web server
+
+=item * Plack::Middleware::Precompressed
+
+serve pre-gzipped content to compression-enabled clients
+
+=item * Plack::Middleware::FileWrap
+
+Wrap file with headers/footers in Plack
+
+=item * Plack::Middleware::Rewrite
+
+mod_rewrite for Plack
+
+=item * Plack::Middleware::RedirectSSL
+
+force all requests to use in-/secure connections
+
+=item * Plack::Middleware::ReverseProxyPath
+
+adjust proxied env to match client-facing
+
+=item * Plack::Middleware::DetectMobileBrowsers
+
+Automatically set a flag in the environment if a mobile client is detected
+
+=item * Plack::Middleware::Auth::AccessToken
+
+Secret access token authentification
+
+=item * Plack::Middleware::DirIndex
+
+Append an index file to request PATH's ending with a /
+
+=item * Plack::Middleware::RequestHeaders
+
+modify HTTP request headers
+
+=item * Plack::Middleware::NeverExpire
+
+set expiration headers far in the future
+
+=item * Plack::Middleware::ETag
+
+Adds automatically an ETag header.
+
+=item * Plack::Middleware::REST
+
+Route PSGI requests for RESTful web applications
+
+=item * Plack::Middleware::ChromeFrame
+
+injects Google Chrome Frame into IE
+
+=item * Plack::Middleware::Debug::TraceENV
+
+debug panel for tracing %ENV
+
+=item * Plack::Client
+
+abstract interface to remote web servers and local PSGI apps
+
+=item * Plack::Middleware::Compile
+
+Compile HAML/SASS/Coffeescript/whatever on demand
+
+=item * Plack::Middleware::Static::Combine
+
+Serve multiple static files combined
+
+=item * Plack::Middleware::CSRFBlock
+
+Block CSRF Attacks with minimal changes to your app
+
+=item * Plack::Middleware::Proxy::Requests
+
+Forward proxy server
+
+=item * Plack::Middleware::SocketIO
+
+Socket.IO Plack middleware
+
+=item * Plack::Middleware::XForwardedFor
+
+Plack middleware to handle X-Forwarded-For headers
+
+=item * Plack::Middleware::HTMLMinify
+
+Plack middleware to minify HTML on-the-fly
+
+=item * Plack::Middleware::HTMLify
+
+Transform a non-html page into html.  
+
+=item * Plack::Middleware::Auth::QueryString
+
+simple query string authentication
+
+=item * Plack::Middleware::ReverseProxy
+
+Supports app to run as a reverse proxy backend
+
+=item * Plack::Middleware::QRCode
+
+Mount QRCode Image service on your Plack application.
+
+=item * Plack::Middleware::Class::Refresh
+
+Refresh your app classes with Class::Refresh before requests
+
+=item * Plack::Middleware::TrafficLog
+
+Log headers and body of HTTP traffic
+
+=item * Plack::Middleware::Auth::OAuth
+
+OAuth signature validation middleware
+
+=item * Plack::Middleware::Log::Minimal
+
+Log::Minimal middleware to prints to psgi.errors
+
+=item * Plack::Middleware::Debug::Ajax
+
+Show log of ajax requests/responses
+
+=item * Plack::Middleware::Auth::Digest
+
+Digest authentication
+
+=item * Plack::Middleware::Header
+
+modify HTTP response headers
+
+=item * Plack::Middleware::File::Less
+
+=item * Plack::Middleware::MemoryUsage
+
+for measuring process memory
+
+=item * Plack::Middleware::Debug::Log4perl
+
+Plack debug panel to show detailed Log4perl debug messages.
+
+=item * Plack::Middleware::ConsoleLogger
+
+Write logs to Firebug or Webkit Inspector
+
+=item * Plack::Middleware::Profiler::NYTProf
+
+Middleware for Profiling a Plack App
+
+=item * Plack::Middleware::HTMLLint
+
+check syntax with HTML::Lint for PSGI application's response HTML
+
+=item * Plack::Middleware::Debug
+
+display information about the current request/response
+
+=item * Padre
+
+Perl Application Development and Refactoring Environment
+
+=item * Task::Padre::Plugins
+
+Get many Plugins of Padre at once
+
+=item * Rex::IO::CMDB
+
+Configuration Management Database
+
+=item * Rex::IO::Server
+
+The webservice component of Rex.IO
+
+=item * Rex::Template::TT
+
+Use Template::Toolkit with Rex
+
+=item * Rex::Bundle
+
+Bundle Perl Libraries
+
+=item * Rex::Apache::Deploy
+
+Configuration Injection module for Rex::Apache::Deploy
+
+=item * Rex::Endpoint::HTTP
+
+Execute Rex over HTTP
+
+=item * Rex::Gearman
+
+=item * Rex::IO::Client
+
+Client Library for Rex::IO::Server
+
+=item * Rex::Augeas
+
+An augeas module for (R)?ex
+
+=item * XML::Parser::REX
+
+XML Shallow Parsing with Regular Expressions
+
+=item * Rex
+
+Remote Execution
+
+=item * Moo
+
+Minimalist Object Orientation (with Moose compatiblity)
+
+=item * MooX::Log::Any
+
+Role to add Log::Any
+
+=item * MooX
+
+Using Moo and MooX:: packages the most lazy way
+
+=item * MooX::Cmd
+
+Giving an easy Moo style way to make command organized CLI apps
+
+=item * MooX::Types::CLike
+
+C-like data types for Moo
+
+=item * MooX::ClassAttribute
+
+declare class attributes Moose-style... but without Moose
+
+=item * MooX::Types::MooseLike::Email
+
+Email address validation type constraint for Moo.
+
+=item * MooX::Options
+
+add option keywords to your object (Mo/Moo/Moose)
+
+=item * MooX::Singleton
+
+turn your Moo class into singleton
+
+=item * MooX::Role::Pluggable
+
+Add a plugin pipeline to your cows
+
+=item * MooX::Types::MooseLike
+
+some Moosish types and a type builder
 
 =back
